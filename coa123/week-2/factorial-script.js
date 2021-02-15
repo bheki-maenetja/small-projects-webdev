c2a4c57ae9dfe10496af4bf6fdf673c3c17ebd53
bheki-maenetja <bhekimaenetja@gmail.com>
2021-02-15 11:28:37 +0000

    completed second lab exercise

---
+++ coa123/week-2/factorial-script.js
@@ -0,0 +1,15 @@
+function factorialCalc() {
+  const num = Number(prompt('Enter your number:'))
+  let result = 1
+  // Task: Calculate factorial of num and store it in result variable
+  function getFac(n) {
+    if (n === 1) {
+      return 1
+    } else {
+      return n * getFac(n - 1)
+    }
+  }
+  result = getFac(num)
+  // end of Task
+  alert(result)
+}
