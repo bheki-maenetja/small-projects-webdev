d1a8056500a813b5154873c20ab894d57e1904ef
bheki-maenetja <bhekimaenetja@gmail.com>
2021-03-15 16:07:44 +0000

    completed main part of week 6 lab

--- coa123/week-6/result.php
+++ coa123/week-6/result.php
@@ -13,19 +13,27 @@
 <?php
 $registeredEmails = array("h.nevisi@lboro.ac.uk","test@test.com","hello@test.com");
 //Task1. get firsname, lastname, backgroundcolor, tablesize, email values from the query string and store them in $firsName, $lastName, $backgroundColor, $tableSize and $email
-
+$firstName = $_REQUEST['firstname'];
+$lastName = $_REQUEST['lastname'];
+$backgroundColor = $_REQUEST['backgroundcolor'];
+$tableSize = $_REQUEST['tablesize'];
+$email = $_REQUEST['email'];
 
 
 
 
 //Task2. greeting to the person using echo, and let them know whether their email address is in our registered email list $registeredEmails (you can use built-in function)
-
+echo 'Hello ' . $firstName;
+echo '<br>';
+if (in_array($email, $registeredEmails)) {
+    echo 'Welcome back!';
+} 
 
 
 
 
 //Task3. Change the background of the table below to the background value that the user has submitted
-echo "<table>";
+echo "<table style='background-color: {$backgroundColor}'>";
 for($i=1;$i<=$tableSize;$i=$i+1){
 	echo '<tr>';
 	for($j=1; $j <= $tableSize; $j++) {
