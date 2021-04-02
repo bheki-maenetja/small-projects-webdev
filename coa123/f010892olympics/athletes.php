45bd61876ea0b8e6ca16674fdc10ca097ddb5eaf
bheki-maenetja <bhekimaenetja@gmail.com>
2021-04-02 15:57:35 +0100

    implemented table generation for task 2 of cw

--- coa123/f010892olympics/athletes.php
+++ coa123/f010892olympics/athletes.php
@@ -6,6 +6,58 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Athletes Task</title>
 </head>
+<style>
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        font-family: helvetica;
+    }
+
+    body {
+        width: 100vw;
+        height: 100vh;
+        background-color: #d4dddd;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .table-container {
+        max-width: 80%;
+        max-height: 90%;
+        margin: 100px auto;
+        overflow: scroll;
+        display: flex;
+        justify-content: flex-start;
+        align-items: flex-start;
+        box-shadow: 0 0 20px 5px black;
+    }
+
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+
+    td, th {
+        padding: 10px;
+        text-align: center;
+    }
+
+    .column-heading {
+        position: sticky;
+        background-color: #4a4a4a;
+        color: white;
+        top: 0;
+    }
+
+    td {
+        background-color: #acc8d4;
+    }
+
+    table {
+        max-height: 100%;
+    }
+</style>
 <body>
 <?php 
     $username = "coa123cycle";
@@ -27,9 +79,27 @@
     $search_result = mysqli_query($conn, $db_query);
 
     if (mysqli_num_rows($search_result) > 0){
+        generateTable();
+    }
+    
+    function generateTable() {
+        global $search_result;
+        echo "<div class='table-container'>";
+        echo "<table>";
+        echo "<tr>";
+        echo "<th class='column-heading'>Name</th>";
+        echo "<th class='column-heading'>Gender</th>";
+        echo "<th class='column-heading'>BMI</th>";
+        echo "</tr>";
         while ($row = mysqli_fetch_array($search_result)){
-            echo $row["name"] . " " . $row["gender"] . " " . $row["BMI"] . "<br>";
+            echo "<tr>";
+            echo "<td>" . $row['name'] . "</td>"; 
+            echo "<td>" . $row['gender'] . "</td>";
+            echo "<td>" . $row['BMI'] . "</td>";
+            echo "</tr>";
         }
+        echo "</table>";
+        echo "</div>";
     }
     mysqli_close($conn);
 ?>
