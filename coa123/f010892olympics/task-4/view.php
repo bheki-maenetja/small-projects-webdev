bff10359c7a55b86bc5206a63bccd6597b71e717
bheki-maenetja <bhekimaenetja@gmail.com>
2021-04-07 10:14:39 +0100

    re-arranged file structure

---
+++ coa123/f010892olympics/task-4/view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task 4</title>
+</head>
+<body>
+    <?php 
+    echo "<h1>Coursework - Task 4</h1>"
+    ?>
+</body>
+</html>
