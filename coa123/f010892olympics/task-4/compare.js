eb063d83eb13cebb906053fe2ee57be652af70cb
bheki-maenetja <bhekimaenetja@gmail.com>
2021-04-08 13:46:35 +0100

    figured out backend data requests

---
+++ coa123/f010892olympics/task-4/compare.js
@@ -0,0 +1,16 @@
+function pageSetup () {
+  var httpRequest = new XMLHttpRequest()
+  httpRequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var dataObj = JSON.parse(this.response)
+      console.log(typeof(dataObj))
+      console.log(dataObj)
+    } else {
+      console.log('SOMETHING IS WRONG')
+    }
+  }
+  httpRequest.open('GET', 'get_data.php', true)
+  httpRequest.send()
+}
+
+window.addEventListener('DOMContentLoaded', pageSetup)
