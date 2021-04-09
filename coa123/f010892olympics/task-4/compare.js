16350b19c3e5a774e889f581da31799a1ba5362d
bheki-maenetja <bhekimaenetja@gmail.com>
2021-04-09 10:20:48 +0100

    building html markup for country comparison page

--- coa123/f010892olympics/task-4/compare.js
+++ coa123/f010892olympics/task-4/compare.js
@@ -1,16 +1,20 @@
 function pageSetup () {
-  var httpRequest = new XMLHttpRequest()
-  httpRequest.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var dataObj = JSON.parse(this.response)
-      console.log(typeof(dataObj))
-      console.log(dataObj)
-    } else {
-      console.log('SOMETHING IS WRONG')
+
+  function getData() {
+    var httpRequest = new XMLHttpRequest()
+    httpRequest.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var dataObj = JSON.parse(this.response)
+        console.log(typeof(dataObj))
+        console.log(dataObj)
+      } else {
+        console.log('SOMETHING IS WRONG')
+      }
     }
+    httpRequest.open('GET', 'get_data.php', true)
+    httpRequest.send()
   }
-  httpRequest.open('GET', 'get_data.php', true)
-  httpRequest.send()
+  getData()
 }
 
 window.addEventListener('DOMContentLoaded', pageSetup)
