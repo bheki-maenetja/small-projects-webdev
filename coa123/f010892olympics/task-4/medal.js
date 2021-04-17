50a8c2381e23f4a9ff3e7bfff1a0cd9eab86a531
bheki-maenetja <bhekimaenetja@gmail.com>
2021-04-17 11:04:18 +0100

    implemented basic get & render; preparing to implement search & sort

--- coa123/f010892olympics/task-4/medal.js
+++ coa123/f010892olympics/task-4/medal.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 function pageHandler() {
   console.log('Let\'s go!!!')
 
@@ -7,6 +8,7 @@ function pageHandler() {
   }
 
   // DOM Variables
+  const tableBody = document.querySelector('tbody')
 
   // Loading Data
   function setCountryData() {
@@ -17,6 +19,39 @@ function pageHandler() {
   }
 
   setCountryData()
+
+  // Rendering Functionality
+  function renderTable() {
+    const searchResults = pageState['searchResults']
+    if (searchResults.length === 0) {
+      tableBody.innerHTML = `
+            <tr>
+                <td colspan=8>Could not find anything matching your search</td>
+            </tr>
+        `
+    } else {
+      tableBody.innerHTML = `
+            ${searchResults.map((country, index) => {
+                return `
+                    <tr>
+                        <td>${index + 1}</td>
+                        <td>${country.country_name}</td>
+                        <td>${country.num_cyclists}</td>
+                        <td>${country.avg_cyclist_age === 0 ? 'n/a' : country.avg_cyclist_age.toFixed(1)}</td>
+                        <td>${country.gold}</td>
+                        <td>${country.silver}</td>
+                        <td>${country.bronze}</td>
+                        <td>${country.total}</td>
+                    </tr>
+                `
+            })}
+        `.replaceAll(',', '')
+    }
+  }
+
+  renderTable()
+
+  // DOM Object Event Listeners
 }
 
 window.addEventListener('DOMContentLoaded', pageHandler)
