b34588f4ba2e608b8410eb462cc5d7f78b98679c
bheki-maenetja <bhekimaenetja@gmail.com>
2021-04-24 10:56:00 +0100

    added comments and refactored code on task 3

--- coa123/f010892olympics/task-3/details.php
+++ coa123/f010892olympics/task-3/details.php
@@ -13,26 +13,50 @@
     $dbname = "coa123cdb";
     $servername = "localhost";
 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    $conn = mysqli_connect($servername, $username, $password, $dbname); // connecting to database
 
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
 
-    $firstDate = $_REQUEST['date_1'];
-    $secondDate = $_REQUEST['date_2'];
+    $firstDate = $_REQUEST['date_1']; // first date value
+    $secondDate = $_REQUEST['date_2']; // second date value
 
     function validateDate($inputDate) {
+        /* 
+        PARAMETERS
+            * inputDate -> string representing a given date
+        RETURN VALUES
+            * boolean value representing whether or not the given string is a valid date
+        WHAT DOES THIS FUNCTION DO?
+            * makes sure that a given date string represents a valid date
+        */
         $dateFormat = date_create_from_format('d/m/Y', $inputDate);
         return $dateFormat && $dateFormat->format('d/m/Y') == $inputDate;
     }
 
     function reformatDate($date) {
+        /* 
+        PARAMETERS
+            * date -> a validated string representing a given date 
+        RETURN VALUES
+            * a date object whose value is a date in the YYYY-MM-DD format
+        WHAT DOES THIS FUNCTION DO?
+            * creates a date object from a valid date string
+        */
         $dateObj = date_create_from_format('d/m/Y', $date);
         return date_format($dateObj, 'Y-m-d');
     }
 
     function getResults() {
+        /* 
+        PARAMETERS
+            * None
+        RETURN VALUES
+            * None
+        WHAT DOES THIS FUNCTION DO?
+            * queries the datebase using the inputted date values renders the results as JSON
+        */
         global $firstDate, $secondDate, $conn;
         $db_query = "SELECT name, country_name, gdp, population from Country join Cyclist on Country.ISO_id = Cyclist.ISO_id where dob BETWEEN '$firstDate' AND '$secondDate'";
         $search_result = mysqli_query($conn, $db_query);
